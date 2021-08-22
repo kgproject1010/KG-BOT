96065a4db6e68a1a532bf3ec6ca5fff0a509fb50
rakaanjay <87686569+rakaanjay@users.noreply.github.com>
2021-08-22 16:35:27 +0700

    Update sangmata.py

--- userbot/modules/sangmata.py
+++ userbot/modules/sangmata.py
@@ -9,16 +9,16 @@ async def lastname(steal):
     if steal.fwd_from:
         return
     if not steal.reply_to_msg_id:
-        await steal.edit("```Mohon Balas Ke Pesan Pengguna.```")
+        await steal.edit("```REPLY JAMETNYA TOT.```")
         return
     message = await steal.get_reply_message()
     chat = "@SangMataInfo_bot"
     user_id = message.sender.id
     id = f"/search_id {user_id}"
     if message.sender.bot:
-        await steal.edit("```Balas Ke Pesan Pengguna Yang Sebenarnya.```")
+        await steal.edit("```REPLY JAMET NYA TOLOL.```")
         return
-    await steal.edit("```Mengambil Informasi Pengguna Tersebut, Mohon Menunggu..```")
+    await steal.edit("```BENTAR NIH GUA AMBIL HISTORY NAMA JAMET..```")
     try:
         async with bot.conversation(chat) as conv:
             try:
@@ -40,7 +40,7 @@ async def lastname(steal):
             if response.text.startswith("No records") or r.text.startswith(
                 "No records"
             ):
-                await steal.edit("```Saya Tidak Menemukan Informasi Pengguna Ini, Pengguna Ini Belum Pernah Mengganti Nama Sebelumnya```")
+                await steal.edit("```GAADA HISTORYNYA AJG,DIA BELUM PERNAH GANTI NAMA TOT```")
                 await steal.client.delete_messages(
                     conv.chat_id, [msg.id, r.id, response.id]
                 )
@@ -52,11 +52,11 @@ async def lastname(steal):
                 conv.chat_id, [msg.id, r.id, response.id, respond.id]
             )
     except TimeoutError:
-        return await steal.edit("`Saya Sedang Sakit Mohon Maaf`")
+        return await steal.edit("`GUA LAGI COVID JANGAN DI GANGGU`")
 
 
 CMD_HELP.update({
     "sangmata":
         "`.sa`\
-          \nUsage: Mendapatkan Riwayat Nama Pengguna."
+          \nUsage: HISTORY NAMA JAMET."
 })
