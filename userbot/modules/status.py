894e399e4b7d043f7385cd399030e28dc273996d
rakaanjay <87686569+rakaanjay@users.noreply.github.com>
2021-08-22 16:37:33 +0700

    Update status.py

--- userbot/modules/status.py
+++ userbot/modules/status.py
@@ -17,7 +17,7 @@ from userbot import TEMP_DOWNLOAD_DIRECTORY
 OFFLINE_TAG = f"{ALIVE_NAME} #OFFLINE"
 ONLINE_TAG = f"{ALIVE_NAME} #ONLINE"
 PROFILE_IMAGE = os.environ.get(
-    "PROFILE_IMAGE", "https://telegra.ph/file/249f27d5b52a87babcb3f.jpg"
+    "PROFILE_IMAGE", "https://telegra.ph/file/de901eba4b8f4e3438ed2.jpg"
 )
 
 
@@ -35,7 +35,7 @@ async def _(event):
     if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):  # pylint:disable=E0602
         os.makedirs(TEMP_DOWNLOAD_DIRECTORY)  # pylint:disable=E0602
     urllib.request.urlretrieve(
-        "https://telegra.ph/file/249f27d5b52a87babcb3f.jpg", "donottouch.jpg"
+        "https://telegra.ph/file/de901eba4b8f4e3438ed2.jpg", "donottouch.jpg"
     )
     photo = "donottouch.jpg"
     if photo:
